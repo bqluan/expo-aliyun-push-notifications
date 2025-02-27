ff41bfd20b40daf4674238078875fa8bfcb1484d
zhaoke <zhaoke@ccpit.org>
2025-02-27 14:00:44 +0800

    运行报错问题，以及往manifest写入权限，project build.gradle、allprojects、repositories写入依赖

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -3,6 +3,7 @@ import {
   withAndroidManifest,
   AndroidConfig,
   ConfigPlugin,
+  withProjectBuildGradle
 } from 'expo/config-plugins';
 
 interface AliPushConfig {
@@ -18,6 +19,7 @@ const withAliyunConfig: ConfigPlugin<AliPushConfig> = (config, { aliPushAppKey, 
 
   config = withAndroidManifest(config, config => {
     const mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(config.modResults);
+    const { manifest } = config.modResults;
 
     AndroidConfig.Manifest.addMetaDataItemToMainApplication(
       mainApplication,
@@ -30,11 +32,50 @@ const withAliyunConfig: ConfigPlugin<AliPushConfig> = (config, { aliPushAppKey, 
       'Ali_Push_App_Secret',
       aliPushAppSecret
     );
+    // 需要添加的权限列表
+    const requiredPermissions = [
+      'POST_NOTIFICATIONS',
+      'ACCESS_NETWORK_STATE',
+      'DOWNLOAD_WITHOUT_NOTIFICATION',
+    ];
+    addPermissionsIfMissing(manifest, requiredPermissions);
 
+    return config;
+  });
+  // 修改 android/build.gradle，添加阿里云和华为的 maven 仓库地址
+  config = withProjectBuildGradle(config, config => {
+    if (!config.modResults.contents.includes("https://maven.aliyun.com/nexus/content/repositories/releases/")) {
+      config.modResults.contents = config.modResults.contents.replace(
+        /allprojects\s*{\s*repositories\s*{/,
+        `allprojects {\n    repositories {\n        maven {\n            url 'https://maven.aliyun.com/nexus/content/repositories/releases/'\n        }\n        maven {\n            url 'https://developer.huawei.com/repo/'\n        }`
+      );
+    }
     return config;
   });
 
   return config;
 };
+
+function addPermissionsIfMissing(manifest: any, permissionNames: string[]): void {
+  if (!manifest['uses-permission']) {
+    manifest['uses-permission'] = [];
+  }
+
+  const permissions = manifest['uses-permission'];
+
+  permissionNames.forEach(permissionName => {
+    const fullPermissionName = `android.permission.${permissionName}`;
+
+    const hasPermission = permissions.some(
+      (perm: any) => perm.$['android:name'] === fullPermissionName
+    );
+
+    if (!hasPermission) {
+      permissions.push({
+        $: { 'android:name': fullPermissionName },
+      });
+    }
+  });
+}
 
 export default withAliyunConfig;
